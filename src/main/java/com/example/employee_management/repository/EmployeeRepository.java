dbc872e7f4c4763051e81260b7404e3374bb3df2
Simiksha <150524224+Simiksha@users.noreply.github.com>
2026-01-23 10:33:33 +0530

    Delete src directory

--- src/main/java/com/example/employee_management/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.employee_management.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.employee_management.model.Employee;
-
-public interface EmployeeRepository extends MongoRepository<Employee, String>{
-} 
